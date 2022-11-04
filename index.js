9608aff5c481f4e70130c82ce5dcebfd70d672f3
Avdhoot-Kurhe <101327844+Avdhoot-Kurhe@users.noreply.github.com>
2022-11-04 12:16:53 +0530

    Index.js file Updated

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req,res)=>{
     res.sendFile("Hello User", "this is application api")
 });
 
+app.get('/about', (req,res)=>{
+    res.sendFile("Wel-Come to  about page")
+});
+app.get('/user', (req,res)=>{
+    res.sendFile("Wel-Come to  user page")
+});
 app.listen(port,()=>{
     console.log(`Server Run On ${port}`)
 })
