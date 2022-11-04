71f43f1c9fe95c0fbf09294e811e84d3e5216f63
Avdhoot-Kurhe <101327844+Avdhoot-Kurhe@users.noreply.github.com>
2022-11-04 12:32:51 +0530

    index file change

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 const port=process.env.port||8000;
 
 app.get('/', (req,res)=>{
-    res.sendFile("Hello User", "this is application api")
+    res.send("Hello User"+" "+"this is application api")
 });
 
 app.get('/about', (req,res)=>{
-    res.sendFile("Wel-Come to  about page")
+    res.send("Welcome to  about page")
 });
 app.get('/user', (req,res)=>{
-    res.sendFile("Wel-Come to  user page")
+    res.send("Wel-Come to  user page")
 });
 app.listen(port,()=>{
     console.log(`Server Run On ${port}`)
